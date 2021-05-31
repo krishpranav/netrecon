f0d8049cdb0754a5dc24d40f644ffde3f3d45642
krishpranav <krisna.pranav@gmail.com>
2021-05-31 11:53:04 +0530

    [+] rtsp screen shot [+]

---
+++ scripts/rtsp_screenshot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ffmpeg -loglevel fatal -i rtsp://$1:$2/ -vframes 1 -r 1 /tmp/badkarma-screenshot-$1-$2.jpg
+base64 /tmp/badkarma-screenshot-$1-$2.jpg
+rm /tmp/badkarma-screenshot-$1-$2.jpg
