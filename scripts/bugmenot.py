f0f953613f44703eb4f5081e221d3c097c1cfa09
krishpranav <krisna.pranav@gmail.com>
2021-05-31 12:08:17 +0530

    [+] bug me not main updated [+]

--- scripts/bugmenot.py
+++ scripts/bugmenot.py
@@ -39,3 +39,36 @@ class BugMeNot:
 
 	def get_account(self, host):
 		return self._get_account(host)
+
+
+if __name__ == '__main__':
+    	parser = optparse.OptionParser()
+
+	parser.add_option("-s", "--site", dest="site", help="The target site")
+	parser.add_option("-t", "--ret_type", dest="ret", default="text", help="The return type(text/json)")
+
+	(options, args) = parser.parse_args()
+
+	if options.site:
+		bug = BugMeNot()
+		accounts = bug.get_account(options.site)
+		if not accounts:
+			print ("No accounts/password for %s found in www.bugmenot.com" % options.site)
+			sys.exit(1)
+
+		if options.ret == 'text':
+			print ("%-30s\t%-20s\t%-5s" % ("Username", "Password", "Success"))
+			line_len = 70
+
+			print ("-" * line_len)
+			for account in accounts:
+				print ("%(username)-30s\t%(password)-20s\t%(stats)-5s" % account)
+
+		elif options.ret == 'json':
+			
+			print (json.dumps(accounts))
+	else:
+		parser.print_help()
+		sys.exit()
+
+	sys.exit()
