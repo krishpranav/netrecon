f9fe479d31409ad9990d1d27d19c84c42d2d9773
krishpranav <krisna.pranav@gmail.com>
2021-05-31 12:03:06 +0530

    [+] _get_account try block updated [+]

--- scripts/bugmenot.py
+++ scripts/bugmenot.py
@@ -14,3 +14,21 @@ class BugMeNot:
         self.regex = u'<dl><dt>Username:</dt><dd><kbd>([^<]*)</kbd></dd>'
         self.regex += u'<dt>Password:</dt><dd><kbd>([^<]*)</kbd></dd>'
         self.regex += u'<dt class="stats">Stats:</dt><dd class="stats"> <ul> <li class="[^"]* [^"]*">([0-9]*%)[^<]*</li>'
+
+
+    def _get_account(self, host):
+        headers = dict()
+        headers['User-Agent'] = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)'
+        headers['Pragma'] = 'no-cache'
+        headers['Cache-Control'] = 'no-cache'
+
+        try:
+            request = urllib.request.Request('http://www.bugmenot.com/view/%s' % host, None, headers) 
+
+            page = urllib.request.urlopen(request).read().decode()
+
+        except Exception as e:
+
+            print(e)            
+            print ('Http Error! Please check the url you input and the network connection')
+            sys.exit()
