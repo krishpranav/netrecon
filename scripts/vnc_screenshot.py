5bff996a520676153dab112d772ebb21fa55ae2d
krishpranav <krisna.pranav@gmail.com>
2021-05-31 12:16:18 +0530

    [+] vnc screen shot functions updated [+]

--- scripts/vnc_screenshot.py
+++ scripts/vnc_screenshot.py
@@ -12,3 +12,16 @@ gi.require_version('GtkVnc', '2.0')
 
 from gi.repository import GtkVnc
 from gi.repository import Gtk
+
+
+def screenshot(dpy):
+	time.sleep(5)
+	pix = dpy.get_pixbuf()
+	pix.savev( "/tmp/netrecon-screenshot"+sys.argv[1]+"-"+sys.argv[2], "png", "", "")
+
+	with open ("/tmp/netrecon-screenshot"+sys.argv[1]+"-"+sys.argv[2], "rb" ) as image:
+		print( base64.b64encode(image.read()).decode() )
+
+	# quit
+	Gtk.main_quit()
+	quit()		
